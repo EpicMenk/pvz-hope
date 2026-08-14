2b92827ed3ca381f8c6b021ac182ed1abb906ab0
EpicMenk <nguyenthitrung3159@gmail.com>
2026-08-14 15:01:40 +0700

    fixed

--- LevelRelated/WaveActions/sunSkySpawnAction.gd
+++ LevelRelated/WaveActions/sunSkySpawnAction.gd
@@ -3,8 +3,14 @@ class_name sunSkySpawnAction
 
 @export var amount : int
 @export_range(0.0, 0.5) var randomJitter : float = 0.0
+var finished : bool = false
 
 func executeAction(context : actionContext):
 	for i in amount:
 		await context.get_tree().create_timer(randf_range(0,randomJitter)).timeout
 		context._sunManager.spawnSkySun()
+	finished = true
+
+func isFinished(context : actionContext):
+	if finished == true:
+		return true
